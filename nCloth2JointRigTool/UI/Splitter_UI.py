dbaa4d15029b83990adf7ae984da09e09687849c
Yixiong Xu <328665042@qq.com>
2018-11-12 10:24:24 +0800

    Splitter UI

---
+++ nCloth2JointRigTool/UI/Splitter_UI.py
@@ -0,0 +1,42 @@
+from PySide2 import QtWidgets, QtCore, QtGui
+
+
+class Splitter(QtWidgets.QWidget):
+    def __init__(self, text=None):
+        """
+        Splitter class for create splitter widget
+        :param text: text between splitter lines
+        """
+        super(Splitter, self).__init__()
+
+        self.setMinimumHeight(2)
+        self.mainLayout = QtWidgets.QHBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.setSpacing(0)
+        self.mainLayout.setAlignment(QtCore.Qt.AlignVCenter)
+
+        firstLine = QtWidgets.QFrame()
+        firstLine.setFrameStyle(QtWidgets.QFrame.HLine)
+        self.mainLayout.addWidget(firstLine)
+
+        if not text:
+            return
+
+        font = QtGui.QFont()
+        font.setBold(True)
+
+        textWidth = QtGui.QFontMetrics(font)
+        width = textWidth.width(text) + 10
+
+        label = QtWidgets.QLabel()
+        label.setText(text)
+        label.setFont(font)
+        label.setMaximumWidth(width)
+        label.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignHCenter)
+
+        self.mainLayout.addWidget(label)
+
+        secondLine = QtWidgets.QFrame()
+        secondLine.setFrameStyle(QtWidgets.QFrame.HLine)
+        self.mainLayout.addWidget(secondLine)
